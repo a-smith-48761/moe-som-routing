0a748ab374f864454d12c92fbde87c592a3ddf7f
Andy Smith <a.smith.48761@gmail.com>
2026-07-12 21:25:44 +0000

    Fix errors in validation script

--- src/utils/runvalidation.py
+++ src/utils/runvalidation.py
@@ -1,6 +1,6 @@
 import sys
 
-from transformers import GemmaTokenizerFast, Gemma3ForCausalLM, Trainer
+from transformers import GemmaTokenizerFast, Gemma3ForCausalLM, Trainer, DataCollatorForLanguageModeling
 from datasets import load_dataset
 
 from ..models.gemma3moe import Gemma3MoEForCausalLM
@@ -9,8 +9,9 @@ from ..models.gemma3moe import Gemma3MoEForCausalLM
 modelId = sys.argv[1] if len(sys.argv) > 1 else "google/gemma-3-270m-it"
 datasetId = sys.argv[2] if len(sys.argv) > 2 else "openai/gsm8k"
 datasetSplit = sys.argv[3] if len(sys.argv) > 3 else "test"
+datasetConfig = sys.argv[4] if len(sys.argv) > 4 else "main"
 
-if model.startswith("google/"):
+if modelId.startswith("google/"):
     # official model, so use the standard class
     model = Gemma3ForCausalLM.from_pretrained(
         modelId,
@@ -28,8 +29,9 @@ tokenizer = GemmaTokenizerFast.from_pretrained (modelId)
 
 trainer = Trainer  (
     model = model,
+    data_collator = DataCollatorForLanguageModeling(tokenizer = tokenizer),
     processing_class = tokenizer
 )
-metrics = trainer.evaluate (eval_dataset = load_dataset(datasetId, split=datasetSplit))
+metrics = trainer.evaluate (eval_dataset = load_dataset(datasetId, datasetConfig, split=datasetSplit))
 
 print (metrics)
